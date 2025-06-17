2c8b9ab6e96cdcdf65ab23ea9d3aca34577c9dd1
sergeirocks100 <31294280+sergeirocks100@users.noreply.github.com>
2025-06-17 15:13:04 -0400

    lets try this

--- scripts/install_byond.sh
+++ scripts/install_byond.sh
@@ -9,7 +9,7 @@ else
     rm -rf "$HOME/BYOND"
     mkdir -p "$HOME/BYOND"
     cd "$HOME/BYOND"
-    curl -H -v "User-Agent: GearStation/1.0 CI Script" "https://www.byond.com/download/build/515/515.1642_byond_linux.zip" -o byond.zip
+    curl -H -v "User-Agent: GearStation/1.0 CI Script" "https://web.archive.org/web/20250515040112/https://www.byond.com/download/build/515/515.1642_byond_linux.zip" -o byond.zip
     unzip byond.zip
     rm byond.zip
     cd byond
